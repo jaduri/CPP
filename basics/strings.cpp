82fd6d7acc415ddb88eae18ef16b56b83f465daa
Joseph Aduwari <tjaduwari@gmail.com>
2021-01-31 10:51:00 +0000

    refactor code

--- basics/strings.cpp
+++ basics/strings.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
-    string sentence = "Hi, I go to school today";
-    string s1 = "I go";
-    string s2 = "I do not go";
+int replaceText(string sentence, string s1, string s2){
 
     cout << sentence << endl;
 
@@ -13,8 +9,17 @@ int main(){
     int places = s1.length() - 1;
 
     sentence.replace(pos, places, s2);
-
     cout << sentence << endl;
+
+    return 0;
+}
+
+int main(){
+    string sentence = "Hi, I go to school today";
+    string s1 = "I go";
+    string s2 = "I do not go";
+
+    replaceText(sentence, s1, s2);
 
     return 0;
 }
